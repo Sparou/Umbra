87c2c80d71809a561c7fc2bfae7adb028fa9765d
limosoque <yuvantipov@edu.hse.ru>
2024-12-25 12:33:38 +0300

    Minor improvements added

--- Source/Umbra/Private/AI/UmbraAIController.cpp
+++ Source/Umbra/Private/AI/UmbraAIController.cpp
@@ -6,6 +6,7 @@
 #include "AI/UmbraAIPerceptionComponent.h"
 #include "BehaviorTree/BehaviorTreeComponent.h"
 #include "BehaviorTree/BlackboardComponent.h"
+#include "Perception/AISense_Sight.h"
 
 AUmbraAIController::AUmbraAIController()
 {
@@ -26,4 +27,18 @@ void AUmbraAIController::OnPercepted(AActor* SourceActor, const FAIStimulus Stim
 {
 	FString Message = "Stimuled by" + Stimulus.Type.Name.ToString() + "strength = " + FString::SanitizeFloat(Stimulus.Strength);
 	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, Message);
+	
+	if(Stimulus.Type == UAISense::GetSenseID(UAISense_Sight::StaticClass()))
+	{
+		if(Stimulus.WasSuccessfullySensed())
+		{
+			Blackboard->SetValueAsObject("CurrentEnemy", SourceActor);
+			Blackboard->SetValueAsVector("PerceptionSourceLocation", Stimulus.StimulusLocation);
+		}
+		else
+		{
+			Blackboard->SetValueAsObject("CurrentEnemy", nullptr);
+			Blackboard->SetValueAsVector("PerceptionSourceLocation", Stimulus.StimulusLocation);
+		}
+	}
 }
